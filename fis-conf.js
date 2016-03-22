672ed7261456ded0d4b00a9dbcc1798a19e49690
baidu <baidu@Richard-HuangdeMacBook-Pro-PHF4A077.local>
2016-03-22 21:33:45 +0800

    0.0.1 压缩，less->css,domin, media===debug

---
+++ fis-conf.js
@@ -0,0 +1,46 @@
+/*
+ * author huangzongzhe hzz780@qq.com
+ * Richard Huang
+ */
+fis.set('project.name', 'fis3-zzbase');
+
+fis.match('*', {
+  useHash: false
+});
+
+
+fis.match('*.less', {
+  // fis-parser-less 插件进行解析
+  parser: fis.plugin('less'),
+  // .less 文件后缀构建后被改成 .css 文件
+  rExt: '.css',
+  useHash: true
+});
+
+// 加 md5
+fis.match('*.{js,css,png}', {
+  useHash: true
+});
+
+fis.match('*.js', {
+  // fis-optimizer-uglify-js 插件进行压缩
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  // fis-optimizer-png-compressor 插件进行压缩
+  optimizer: fis.plugin('png-compressor')
+});
+
+// debug时，不要压缩使用，执行fis release debug
+fis.media('debug').match('*.{js,less,css,png}', {
+  useHash: false,
+  useSprite: false,
+  optimizer: null
+  // ,domain: 'Users/baidu/Desktop/myGit/fis3-zzBase/output'
+})
